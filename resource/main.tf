2d16e289d263655f8c6c627f7436383fc4b5d036
Tanvi-source <tanvibhusari2003@gmail.com>
2025-11-16 06:09:29 +0000

    Add backend S3 bucket and DynamoDB lock table

---
+++ resource/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "backend"     # your backend bucket name
+  force_destroy = false
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
