9ac9614ee843d197a6c9cae61cb7023c01fb598d
Tanvi-source <tanvibhusari2003@gmail.com>
2025-11-16 16:32:24 +0530

    Refactor AWS resource definitions in main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-  key_name      = var.key
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.key
   associate_public_ip_address = true
 
   tags = {
-    
     Name = "AWS-instance"
   }
 }
