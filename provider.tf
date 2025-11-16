5846ab87bc64a5f4b761460cc58df7de36857785
Tanvi-source <tanvibhusari2003@gmail.com>
2025-11-16 17:26:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  region     = "ap-south-1" # Mumbai region
-  access_key = "AKIA2WVSLFQSYYPNUL6N"
-  secret_key = "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"
+  region     = "ap-south-1"
 }
